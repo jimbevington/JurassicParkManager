f52279e3e21b2294f8bb453c47e455bd8239fcfe
Jim Bevington <jim.bevington@hotmail.com>
2018-03-23 16:52:43 +0000

    removed hunger as a property that is passed into Carnivore constructor

--- src/main/java/com/codeclan/jurassicpark/db/models/Carnivore.java
+++ src/main/java/com/codeclan/jurassicpark/db/models/Carnivore.java
@@ -7,13 +7,15 @@ public class Carnivore extends Dinosaur {
     public Carnivore() {
     }
 
-    public Carnivore(SpeciesType species, String name, int age, int danger, Paddock paddock, int hunger) {
+    public Carnivore(SpeciesType species, String name, int age, int danger, Paddock paddock) {
         super(species, name, age, danger, paddock);
-        this.hunger = hunger;
+        this.hunger = 0;
     }
 
     public void getFed(){
 //        feeding Dinosaur resets Hunger to 0
         this.hunger = 0;
     }
+
+//    we need a way for the Carnivore to get Hungry
 }
