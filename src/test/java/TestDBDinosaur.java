257cdc8027f5acd5b7bfd8eb5aa306daceaaef41
Jim Bevington <jim.bevington@hotmail.com>
2018-03-23 16:41:31 +0000

    added setup to Dinosaur test

--- src/test/java/TestDBDinosaur.java
+++ src/test/java/TestDBDinosaur.java
@@ -1,14 +1,23 @@
 import com.codeclan.jurassicpark.db.models.Carnivore;
 import com.codeclan.jurassicpark.db.models.Herbivore;
+import com.codeclan.jurassicpark.db.models.Paddock;
+import com.codeclan.jurassicpark.db.models.SpeciesType;
 import org.junit.Before;
 
 public class TestDBDinosaur {
 
+    private Paddock paddock;
     private Carnivore carnivore;
     private Herbivore herbivore;
 
     @Before
     public void setUp() throws Exception {
-        carnivore = new Carnivore()
+        carnivore = new Carnivore(SpeciesType.VELOCIRAPTOR, "Gerald", 5, 100, 90, paddock);
     }
+
+//    test starts off secure
+
+//    test starts off not hungry
+
+//    can get Fed
 }
