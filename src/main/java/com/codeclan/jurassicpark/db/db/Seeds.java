38d0843c13b87104f46c5302938c588b8769f609
Christiebelle <cslester@outlook.com>
2018-03-23 17:27:44 +0000

    Paddocks added

--- src/main/java/com/codeclan/jurassicpark/db/db/Seeds.java
+++ src/main/java/com/codeclan/jurassicpark/db/db/Seeds.java
@@ -14,16 +14,25 @@ public class Seeds {
         Paddock paddock1 = new Paddock("Green Gully", 10);
         DBHelper.saveOrUpdate(paddock1);
 
-        Carnivore carnivore1 = new Carnivore(SpeciesType.TREX, "Fluffy", 30, 70, paddock1);
+        Paddock paddock2 = new Paddock("Arid Desert", 10);
+        DBHelper.saveOrUpdate(paddock2);
+
+        Paddock paddock3 = new Paddock("Misty Mountains", 10);
+        DBHelper.saveOrUpdate(paddock3);
+
+        Paddock paddock4 = new Paddock("The Great Plains", 10);
+        DBHelper.saveOrUpdate(paddock4);
+
+        Carnivore carnivore1 = new Carnivore(SpeciesType.TREX, "Fluffy", 30, 70);
         DBHelper.saveOrUpdate(carnivore1);
 
-        Carnivore carnivore2 = new Carnivore(SpeciesType.VELOCIRAPTOR, "Blue", 4, 50, paddock1);
+        Carnivore carnivore2 = new Carnivore(SpeciesType.VELOCIRAPTOR, "Blue", 4, 50);
         DBHelper.saveOrUpdate(carnivore2);
 
-        Carnivore carnivore3 = new Carnivore(SpeciesType.VELOCIRAPTOR, "Athena", 10, 90, paddock1);
+        Carnivore carnivore3 = new Carnivore(SpeciesType.VELOCIRAPTOR, "Athena", 10, 90);
         DBHelper.saveOrUpdate(carnivore3);
 
-        Carnivore carnivore4 = new Carnivore(SpeciesType.TREX, "Trevor", 16, 70, paddock1);
+        Carnivore carnivore4 = new Carnivore(SpeciesType.TREX, "Trevor", 16, 70);
         DBHelper.saveOrUpdate(carnivore4);
     }
 }
