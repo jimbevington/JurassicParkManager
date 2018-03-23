cce60749623fafb2a4a05dddcc4dbcd63c8c5d6e
Christiebelle <cslester@outlook.com>
2018-03-23 17:30:57 +0000

    Herbivores added

--- src/main/java/com/codeclan/jurassicpark/db/db/Seeds.java
+++ src/main/java/com/codeclan/jurassicpark/db/db/Seeds.java
@@ -3,7 +3,6 @@ package com.codeclan.jurassicpark.db.db;
 import com.codeclan.jurassicpark.db.models.Carnivore;
 import com.codeclan.jurassicpark.db.models.Herbivore;
 import com.codeclan.jurassicpark.db.models.SpeciesType;
-import sun.security.pkcs11.Secmod;
 
 public class Seeds {
 
@@ -23,6 +22,7 @@ public class Seeds {
         Paddock paddock4 = new Paddock("The Great Plains", 10);
         DBHelper.saveOrUpdate(paddock4);
 
+
         Carnivore carnivore1 = new Carnivore(SpeciesType.TREX, "Fluffy", 30, 70);
         DBHelper.saveOrUpdate(carnivore1);
 
@@ -34,5 +34,18 @@ public class Seeds {
 
         Carnivore carnivore4 = new Carnivore(SpeciesType.TREX, "Trevor", 16, 70);
         DBHelper.saveOrUpdate(carnivore4);
+
+
+        Herbivore herbivore1 = new Herbivore(SpeciesType.BRACHIOSAURUS, "Betty", 20, 50);
+        DBHelper.saveOrUpdate(herbivore1);
+
+        Herbivore herbivore2 = new Herbivore(SpeciesType.BRACHIOSAURUS, "Barbara", 18, 50);
+        DBHelper.saveOrUpdate(herbivore2);
+
+        Herbivore herbivore3 = new Herbivore(SpeciesType.TRICERATOPS, "Terry", 37, 80);
+        DBHelper.saveOrUpdate(herbivore3);
+
+        Herbivore herbivore4 = new Herbivore(SpeciesType.TRICERATOPS, "Thomas", 42, 80);
+        DBHelper.saveOrUpdate(herbivore4);
     }
 }
