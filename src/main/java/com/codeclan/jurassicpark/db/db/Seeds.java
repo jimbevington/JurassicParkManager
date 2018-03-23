f926ae99a5da6b0a0006be6d7c3c080fced30e56
Christiebelle <cslester@outlook.com>
2018-03-23 17:14:27 +0000

    Seeds file created

---
+++ src/main/java/com/codeclan/jurassicpark/db/db/Seeds.java
@@ -0,0 +1,15 @@
+package com.codeclan.jurassicpark.db.db;
+
+import com.codeclan.jurassicpark.db.models.Carnivore;
+import com.codeclan.jurassicpark.db.models.Herbivore;
+
+public class Seeds {
+
+    public static void seedData{
+        DBHelper.deleteAll(Carnivore.class);
+        DBHelper.deleteAll(Herbivore.class);
+
+
+
+    }
+}
