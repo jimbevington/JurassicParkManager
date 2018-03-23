a6f8014858858175f38f85c989aceaf26478d65b
Jim Bevington <jim.bevington@hotmail.com>
2018-03-23 21:37:21 +0000

    added paddock property to constructors in Seeds. removed TestPaddock

--- src/main/java/com/codeclan/jurassicpark/db/db/Seeds.java
+++ src/main/java/com/codeclan/jurassicpark/db/db/Seeds.java
@@ -24,29 +24,29 @@ public class Seeds {
         DBHelper.saveOrUpdate(paddock4);
 
 
-        Carnivore carnivore1 = new Carnivore(SpeciesType.TREX, "Fluffy", 30, 70);
+        Carnivore carnivore1 = new Carnivore(SpeciesType.TREX, "Fluffy", 30, 70, paddock1);
         DBHelper.saveOrUpdate(carnivore1);
 
-        Carnivore carnivore2 = new Carnivore(SpeciesType.VELOCIRAPTOR, "Blue", 4, 50);
+        Carnivore carnivore2 = new Carnivore(SpeciesType.VELOCIRAPTOR, "Blue", 4, 50, paddock2);
         DBHelper.saveOrUpdate(carnivore2);
 
-        Carnivore carnivore3 = new Carnivore(SpeciesType.VELOCIRAPTOR, "Athena", 10, 90);
+        Carnivore carnivore3 = new Carnivore(SpeciesType.VELOCIRAPTOR, "Athena", 10, 90, paddock2);
         DBHelper.saveOrUpdate(carnivore3);
 
-        Carnivore carnivore4 = new Carnivore(SpeciesType.TREX, "Trevor", 16, 70);
+        Carnivore carnivore4 = new Carnivore(SpeciesType.TREX, "Trevor", 16, 70, paddock1);
         DBHelper.saveOrUpdate(carnivore4);
 
 
-        Herbivore herbivore1 = new Herbivore(SpeciesType.BRACHIOSAURUS, "Betty", 20, 50);
+        Herbivore herbivore1 = new Herbivore(SpeciesType.BRACHIOSAURUS, "Betty", 20, 50, paddock3);
         DBHelper.saveOrUpdate(herbivore1);
 
-        Herbivore herbivore2 = new Herbivore(SpeciesType.BRACHIOSAURUS, "Barbara", 18, 50);
+        Herbivore herbivore2 = new Herbivore(SpeciesType.BRACHIOSAURUS, "Barbara", 18, 50, paddock4);
         DBHelper.saveOrUpdate(herbivore2);
 
-        Herbivore herbivore3 = new Herbivore(SpeciesType.TRICERATOPS, "Terry", 37, 80);
+        Herbivore herbivore3 = new Herbivore(SpeciesType.TRICERATOPS, "Terry", 37, 80, paddock3);
         DBHelper.saveOrUpdate(herbivore3);
 
-        Herbivore herbivore4 = new Herbivore(SpeciesType.TRICERATOPS, "Thomas", 42, 80);
+        Herbivore herbivore4 = new Herbivore(SpeciesType.TRICERATOPS, "Thomas", 42, 80, paddock4);
         DBHelper.saveOrUpdate(herbivore4);
     }
 }
