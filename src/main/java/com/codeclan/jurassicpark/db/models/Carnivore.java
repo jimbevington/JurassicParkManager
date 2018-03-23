53f1f572d10ce2da7bf01c3fbb87b02a4cf94e61
Jim Bevington <jim.bevington@hotmail.com>
2018-03-23 16:50:23 +0000

    added Carnivore class constructor, properties and method

--- src/main/java/com/codeclan/jurassicpark/db/models/Carnivore.java
+++ src/main/java/com/codeclan/jurassicpark/db/models/Carnivore.java
@@ -1,4 +1,19 @@
 package com.codeclan.jurassicpark.db.models;
 
-public class Carnivore {
+public class Carnivore extends Dinosaur {
+
+    private int hunger;
+
+    public Carnivore() {
+    }
+
+    public Carnivore(SpeciesType species, String name, int age, int danger, Paddock paddock, int hunger) {
+        super(species, name, age, danger, paddock);
+        this.hunger = hunger;
+    }
+
+    public void getFed(){
+//        feeding Dinosaur resets Hunger to 0
+        this.hunger = 0;
+    }
 }
