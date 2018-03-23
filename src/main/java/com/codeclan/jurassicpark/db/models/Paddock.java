77e6ff8945f8cf4a2a352b19689e3f6dbeb1ae12
Jim Bevington <jim.bevington@hotmail.com>
2018-03-23 17:14:26 +0000

    added Paddock properties, constructors and getters/setters

--- src/main/java/com/codeclan/jurassicpark/db/models/Paddock.java
+++ src/main/java/com/codeclan/jurassicpark/db/models/Paddock.java
@@ -1,4 +1,61 @@
 package com.codeclan.jurassicpark.db.models;
 
+import java.util.Set;
+
 public class Paddock {
+
+    private int id;
+    private String name;
+    private int capacity;
+    private boolean open;
+    private Set<Dinosaur> dinosaurs;
+
+    public Paddock() {
+    }
+
+    public Paddock(String name, int capacity) {
+        this.name = name;
+        this.capacity = capacity;
+        this.open = true;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public boolean isOpen() {
+        return open;
+    }
+
+    public void setOpen(boolean open) {
+        this.open = open;
+    }
+
+    public Set<Dinosaur> getDinosaurs() {
+        return dinosaurs;
+    }
+
+    public void setDinosaurs(Set<Dinosaur> dinosaurs) {
+        this.dinosaurs = dinosaurs;
+    }
 }
