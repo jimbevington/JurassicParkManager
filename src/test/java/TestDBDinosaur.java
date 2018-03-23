a5dd7951b7aa547cea89f6782901784518669575
Jim Bevington <jim.bevington@hotmail.com>
2018-03-23 16:37:07 +0000

    added Species Type ENUM

--- src/test/java/TestDBDinosaur.java
+++ src/test/java/TestDBDinosaur.java
@@ -1,2 +1,14 @@
+import com.codeclan.jurassicpark.db.models.Carnivore;
+import com.codeclan.jurassicpark.db.models.Herbivore;
+import org.junit.Before;
+
 public class TestDBDinosaur {
+
+    private Carnivore carnivore;
+    private Herbivore herbivore;
+
+    @Before
+    public void setUp() throws Exception {
+        carnivore = new Carnivore()
+    }
 }
