f36efc76e7b17f7923cb237c6e720fc0e216a3cc
Christiebelle <cslester@outlook.com>
2018-03-23 17:23:39 +0000

    More carnivores added

--- src/main/java/com/codeclan/jurassicpark/db/db/Seeds.java
+++ src/main/java/com/codeclan/jurassicpark/db/db/Seeds.java
@@ -17,13 +17,13 @@ public class Seeds {
         Carnivore carnivore1 = new Carnivore(SpeciesType.TREX, "Fluffy", 30, 70, paddock1);
         DBHelper.saveOrUpdate(carnivore1);
 
-        Carnivore carnivore1 = new Carnivore(SpeciesType.TREX, "Fluffy", 30, 70, paddock1);
-        DBHelper.saveOrUpdate(carnivore1);
+        Carnivore carnivore2 = new Carnivore(SpeciesType.VELOCIRAPTOR, "Blue", 4, 50, paddock1);
+        DBHelper.saveOrUpdate(carnivore2);
 
-        Carnivore carnivore1 = new Carnivore(SpeciesType.TREX, "Fluffy", 30, 70, paddock1);
-        DBHelper.saveOrUpdate(carnivore1);
+        Carnivore carnivore3 = new Carnivore(SpeciesType.VELOCIRAPTOR, "Athena", 10, 90, paddock1);
+        DBHelper.saveOrUpdate(carnivore3);
 
-        Carnivore carnivore1 = new Carnivore(SpeciesType.TREX, "Fluffy", 30, 70, paddock1);
-        DBHelper.saveOrUpdate(carnivore1);
+        Carnivore carnivore4 = new Carnivore(SpeciesType.TREX, "Trevor", 16, 70, paddock1);
+        DBHelper.saveOrUpdate(carnivore4);
     }
 }
