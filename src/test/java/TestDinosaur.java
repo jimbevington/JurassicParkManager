5fd0afbee9d672b9ad467682abb2be5017cd1352
Jim Bevington <jim.bevington@hotmail.com>
2018-03-23 21:39:11 +0000

    added paddock to Carnivore constructor in TestDinosaur

--- src/test/java/TestDinosaur.java
+++ src/test/java/TestDinosaur.java
@@ -16,7 +16,7 @@ public class TestDinosaur {
     @Before
     public void setUp() throws Exception {
         paddock = new Paddock("Green Gully", 10);
-        carnivore = new Carnivore(SpeciesType.VELOCIRAPTOR, "Gerald", 5, 90);
+        carnivore = new Carnivore(SpeciesType.VELOCIRAPTOR, "Gerald", 5, 90, paddock);
     }
 
 //    can get Fed
