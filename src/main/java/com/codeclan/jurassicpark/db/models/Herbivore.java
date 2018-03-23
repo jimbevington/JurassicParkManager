2eb25f90fa134575263794cca99da61ead113b65
Jim Bevington <jim.bevington@hotmail.com>
2018-03-23 21:35:28 +0000

    added paddock property to Carnivor and Herbivore super construtors

--- src/main/java/com/codeclan/jurassicpark/db/models/Herbivore.java
+++ src/main/java/com/codeclan/jurassicpark/db/models/Herbivore.java
@@ -5,8 +5,8 @@ public class Herbivore extends Dinosaur{
     public Herbivore() {
     }
 
-    public Herbivore(SpeciesType species, String name, int age, int danger) {
-        super(species, name, age, danger);
+    public Herbivore(SpeciesType species, String name, int age, int danger, Paddock paddock) {
+        super(species, name, age, danger, paddock);
     }
 
 }
