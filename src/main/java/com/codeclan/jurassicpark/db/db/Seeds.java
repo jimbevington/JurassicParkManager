dcf350e37826a8fec9dd9d05c334c2121d9c958b
Christiebelle <cslester@outlook.com>
2018-03-23 17:21:24 +0000

    Carnivore seeds added

--- src/main/java/com/codeclan/jurassicpark/db/db/Seeds.java
+++ src/main/java/com/codeclan/jurassicpark/db/db/Seeds.java
@@ -2,6 +2,8 @@ package com.codeclan.jurassicpark.db.db;
 
 import com.codeclan.jurassicpark.db.models.Carnivore;
 import com.codeclan.jurassicpark.db.models.Herbivore;
+import com.codeclan.jurassicpark.db.models.SpeciesType;
+import sun.security.pkcs11.Secmod;
 
 public class Seeds {
 
@@ -9,6 +11,19 @@ public class Seeds {
         DBHelper.deleteAll(Carnivore.class);
         DBHelper.deleteAll(Herbivore.class);
 
+        Paddock paddock1 = new Paddock("Green Gully", 10);
+        DBHelper.saveOrUpdate(paddock1);
 
+        Carnivore carnivore1 = new Carnivore(SpeciesType.TREX, "Fluffy", 30, 70, paddock1);
+        DBHelper.saveOrUpdate(carnivore1);
+
+        Carnivore carnivore1 = new Carnivore(SpeciesType.TREX, "Fluffy", 30, 70, paddock1);
+        DBHelper.saveOrUpdate(carnivore1);
+
+        Carnivore carnivore1 = new Carnivore(SpeciesType.TREX, "Fluffy", 30, 70, paddock1);
+        DBHelper.saveOrUpdate(carnivore1);
+
+        Carnivore carnivore1 = new Carnivore(SpeciesType.TREX, "Fluffy", 30, 70, paddock1);
+        DBHelper.saveOrUpdate(carnivore1);
     }
 }
