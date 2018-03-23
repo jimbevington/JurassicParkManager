cc66e9386700ab462535496bf7cdee6c38d5febc
Jim Bevington <jim.bevington@hotmail.com>
2018-03-23 17:07:01 +0000

    added Herbivore class

--- src/main/java/com/codeclan/jurassicpark/db/models/Herbivore.java
+++ src/main/java/com/codeclan/jurassicpark/db/models/Herbivore.java
@@ -1,4 +1,12 @@
 package com.codeclan.jurassicpark.db.models;
 
-public class Herbivore {
+public class Herbivore extends Dinosaur{
+
+    public Herbivore() {
+    }
+
+    public Herbivore(SpeciesType species, String name, int age, int danger, Paddock paddock) {
+        super(species, name, age, danger, paddock);
+    }
+
 }
