524e77372ffbe19461f96a260f43cd78ecf8195b
Jim Bevington <jim.bevington@hotmail.com>
2018-03-26 20:01:09 +0100

    added Visitor class with Annotations

---
+++ src/main/java/com/codeclan/jurassicpark/db/models/Visitor.java
@@ -0,0 +1,49 @@
+package com.codeclan.jurassicpark.db.models;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="visitors")
+public class Visitor {
+
+    private int id;
+    private String name;
+    private Paddock paddock;
+
+    public Visitor() {
+    }
+
+    public Visitor(String name) {
+        this.name = name;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Column(name="name")
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name="paddock_id")
+    public Paddock getPaddock() {
+        return paddock;
+    }
+
+    public void setPaddock(Paddock paddock) {
+        this.paddock = paddock;
+    }
+}
