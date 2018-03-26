2d4bbce1753d6db4cdd2112c8817fe60caa324d5
Jim Bevington <jim.bevington@hotmail.com>
2018-03-26 14:01:06 +0100

    added view route to PaddockController

--- src/main/java/com/codeclan/jurassicpark/db/controllers/PaddockController.java
+++ src/main/java/com/codeclan/jurassicpark/db/controllers/PaddockController.java
@@ -4,6 +4,7 @@ import com.codeclan.jurassicpark.db.db.DBDinosaur;
 import com.codeclan.jurassicpark.db.db.DBHelper;
 import com.codeclan.jurassicpark.db.db.DBPaddock;
 import com.codeclan.jurassicpark.db.models.Paddock;
+import com.sun.org.apache.xpath.internal.operations.Mod;
 import spark.ModelAndView;
 import spark.template.velocity.VelocityTemplateEngine;
 
@@ -33,7 +34,27 @@ public class PaddockController {
             return new ModelAndView(model, "templates/layout.vtl");
 
         }, new VelocityTemplateEngine());
+
+        //    view Paddock with Details
+        get("/paddocks/:id", (req, res) -> {
+
+            Map<String, Object> model = new HashMap<>();
+            model.put("template", "templates/paddocks/view/vtl");
+
+            int paddockId = Integer.parseInt(req.params(":id"));
+            Paddock paddock = DBHelper.find(Paddock.class, paddockId);
+            model.put("paddock", paddock);
+
+            return new ModelAndView(model, "templates/layout.vtl");
+
+        }, new VelocityTemplateEngine());
+
+//    update Paddock
+//    add Dinosaurs
+
     }
+
+
 
 
 }
