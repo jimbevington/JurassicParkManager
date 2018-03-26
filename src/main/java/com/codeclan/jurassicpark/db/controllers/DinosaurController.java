0ea02ea9e6d8f7face7457768287fdd632889a31
Christiebelle <cslester@outlook.com>
2018-03-26 13:24:43 +0100

    dino controller created

--- src/main/java/com/codeclan/jurassicpark/db/controllers/DinosaurController.java
+++ src/main/java/com/codeclan/jurassicpark/db/controllers/DinosaurController.java
@@ -1,4 +1,34 @@
 package com.codeclan.jurassicpark.db.controllers;
 
+import com.codeclan.jurassicpark.db.db.DBHelper;
+import com.codeclan.jurassicpark.db.models.Dinosaur;
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static spark.Spark.get;
+
 public class DinosaurController {
+
+    public DinosaurController() {
+        this.setupEndpoints();
+    }
+
+    private void setupEndpoints() {
+
+        get("/dinosaurs", (req, res) -> {
+
+            Map<String, Object> model = new HashMap<>();
+            model.put("template", "templates/dinosaurs/index.vtl");
+
+            List<Dinosaur> dinosaurs = DBHelper.getAll(Dinosaur.class);
+            model.put("dinosaurs", dinosaurs);
+
+            return new ModelAndView(model, "templates/layout.vtl");
+
+        }, new VelocityTemplateEngine());
+    }
 }
