0ce2d40bbb2e9317bad273ee6590da8e8097ba32
Jim Bevington <jim.bevington@hotmail.com>
2018-03-26 12:37:39 +0100

    added DBPaddock with getDinoCount() method

--- src/main/java/com/codeclan/jurassicpark/db/db/DBPaddock.java
+++ src/main/java/com/codeclan/jurassicpark/db/db/DBPaddock.java
@@ -1,9 +1,15 @@
 package com.codeclan.jurassicpark.db.db;
 
+import com.codeclan.jurassicpark.db.models.Dinosaur;
 import com.codeclan.jurassicpark.db.models.Paddock;
+
+import java.util.List;
 
 public class DBPaddock {
 
-    public static getDinoCount(Paddock paddock)
+    public static int getDinoCount(Paddock paddock){
+        List<Dinosaur> dinosaurs = DBHelper.getPaddocksDinosaurs(paddock);
+        return dinosaurs.size();
+    }
 
 }
