193bac8b22a9dc731e544fe71a9e621258d76c75
Jim Bevington <jim.bevington@hotmail.com>
2018-03-27 16:54:52 +0100

    removed comment

--- src/main/java/com/codeclan/jurassicpark/db/db/DBDinosaur.java
+++ src/main/java/com/codeclan/jurassicpark/db/db/DBDinosaur.java
@@ -159,10 +159,4 @@ public class DBDinosaur {
 
 
 
-////    public static String species(Dinosaur dinosaur){
-////        Set<SpeciesType> speciesTypes = new HashSet<>();
-////
-////        }
-//    }
-
 }
