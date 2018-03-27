fe6cc49737b3b2145d1e7c05f7f78a3254cd40eb
Christiebelle <cslester@outlook.com>
2018-03-27 14:08:57 +0100

    ses moved into class

--- src/main/java/com/codeclan/jurassicpark/db/models/Carnivore.java
+++ src/main/java/com/codeclan/jurassicpark/db/models/Carnivore.java
@@ -35,14 +35,8 @@ public class Carnivore extends Dinosaur {
     }
 
 //    we need a way for the Carnivore to get Hungry
-
-    Runnable hungerIncrease = new Runnable() {
         public int increaseHunger() {
             hunger += 1;
             return hunger;
         }
-    }
-    ScheduledExecutorService exec = Executors.newScheduledThreadPool(1);
-    exec.scheduleAtFixedRate(hungerIncrease , 0, 1, TimeUnit.MINUTES);
-
 }
--- src/main/java/com/codeclan/jurassicpark/db/models/runPark.java
+++ src/main/java/com/codeclan/jurassicpark/db/models/runPark.java
@@ -1,4 +1,17 @@
 package com.codeclan.jurassicpark.db.models;
 
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+
 public class runPark {
+
+    //timed method for increasing hunger
+    Runnable hungerIncrease = new Runnable() {
+        public int increaseHunger() {
+            hunger += 1;
+            return hunger;
+        }
+    }
+    ScheduledExecutorService exec = Executors.newScheduledThreadPool(1);
+    exec.scheduleAtFixedRate(hungerIncrease , 0, 1, TimeUnit.MINUTES);
 }
