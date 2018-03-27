4537159ea7ca701b263d8000c4ce1f5a6993d1fa
Jim Bevington <jim.bevington@hotmail.com>
2018-03-27 14:07:20 +0100

    removed comment

--- src/main/java/com/codeclan/jurassicpark/db/db/DBPaddock.java
+++ src/main/java/com/codeclan/jurassicpark/db/db/DBPaddock.java
@@ -114,6 +114,7 @@ public class DBPaddock {
         DBHelper.saveOrUpdate(paddock);
     }
 
+
     public static int getVisitorCount(Paddock paddock){
         List<Visitor> visitors = DBHelper.getPaddocksVisitors(paddock);
         return visitors.size();
