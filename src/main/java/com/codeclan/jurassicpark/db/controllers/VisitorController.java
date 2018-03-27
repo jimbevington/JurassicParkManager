cbaa58d10f04fd4c2d1fbc103842c9a015756291
Jim Bevington <jim.bevington@hotmail.com>
2018-03-27 10:14:51 +0100

    added /visitors route to VisitorsController

---
+++ src/main/java/com/codeclan/jurassicpark/db/controllers/VisitorController.java
@@ -0,0 +1,36 @@
+package com.codeclan.jurassicpark.db.controllers;
+
+import com.codeclan.jurassicpark.db.db.DBHelper;
+import com.codeclan.jurassicpark.db.models.Paddock;
+import com.codeclan.jurassicpark.db.models.Visitor;
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static spark.Spark.get;
+
+public class VisitorController {
+
+    public VisitorController() {
+        this.setupEndpoints();
+    }
+
+    private void setupEndpoints() {
+
+        get("/visitors", (req, res) -> {
+
+            Map<String, Object> model = new HashMap<>();
+            model.put("template", "templates/visitors/index.vtl");
+
+            List<Visitor> visitors = DBHelper.getAll(Visitor.class);
+            model.put("visitors", visitors);
+
+            return new ModelAndView(model, "templates/layout.vtl");
+
+        }, new VelocityTemplateEngine());
+
+    }
+}
