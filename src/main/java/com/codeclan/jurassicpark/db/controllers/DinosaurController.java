a20e4f49fee069f28298f7f513ff5310f9435986
Christiebelle <cslester@outlook.com>
2018-03-27 10:25:33 +0100

    get and post methods added for new dino

--- src/main/java/com/codeclan/jurassicpark/db/controllers/DinosaurController.java
+++ src/main/java/com/codeclan/jurassicpark/db/controllers/DinosaurController.java
@@ -69,7 +69,31 @@ public class DinosaurController {
         }, new VelocityTemplateEngine());
 
 
+        get ("/dinosaurs/new", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+//            String loggedInUser = LoginController.getLoggedInUserName(req, res);
+//            model.put("user", loggedInUser);
+            List<Paddock> paddocks = DBHelper.getAll(Paddock.class);
+            SpeciesType[] species = SpeciesType.values();
+            model.put("paddocks", paddocks);
+            model.put("species", species);
+            model.put("template", "templates/dinosaurs/create.vtl");
+            return new ModelAndView(model, "templates/layout.vtl");
+        }, new VelocityTemplateEngine());
 
+        post ("/dinosaurs", (req, res) -> {
+            int paddockId = Integer.parseInt(req.queryParams("paddock"));
+            Paddock paddock = DBHelper.find(Paddock.class, paddockId);
+            String species = req.queryParams("species");
+            String name = req.queryParams("name");
+            int age = Integer.parseInt(req.queryParams("age"));
+            int danger = Integer.parseInt(req.queryParams("danger"));
+            newdino = DBDinosaur
+            Dinosaur dinosaur = new Dinosaur(species, name, age, paddock, danger);
+            DBHelper.saveOrUpdate(dinosaur);
+            res.redirect("/dinosaurs");
+            return null;
+        }, new VelocityTemplateEngine());
 
         post ("/dinosaurs/:id/remove", (req, res) -> {
             int intId = Integer.parseInt(req.params(":id"));
