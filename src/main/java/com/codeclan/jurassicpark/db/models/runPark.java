4280cf5c1df5693765d586f513b16c6b66588642
Christiebelle <cslester@outlook.com>
2018-03-27 14:33:43 +0100

    timer attempts removed

--- src/main/java/com/codeclan/jurassicpark/db/models/runPark.java
+++ src/main/java/com/codeclan/jurassicpark/db/models/runPark.java
@@ -5,13 +5,5 @@ import java.util.concurrent.ScheduledExecutorService;
 
 public class runPark {
 
-    //timed method for increasing hunger
-    Runnable hungerIncrease = new Runnable() {
-        public int increaseHunger() {
-            hunger += 1;
-            return hunger;
-        }
-    }
-    ScheduledExecutorService exec = Executors.newScheduledThreadPool(1);
-    exec.scheduleAtFixedRate(hungerIncrease , 0, 1, TimeUnit.MINUTES);
 }
+
