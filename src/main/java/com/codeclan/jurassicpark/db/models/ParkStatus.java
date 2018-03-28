14bce84b31032b69050f584aa0033c8ae381e7b2
Christiebelle <cslester@outlook.com>
2018-03-28 15:06:19 +0100

    enum with status messages created

---
+++ src/main/java/com/codeclan/jurassicpark/db/models/ParkStatus.java
@@ -0,0 +1,16 @@
+package com.codeclan.jurassicpark.db.models;
+
+public enum ParkStatus {
+    HIDEYOKIDSHIDEYOWIFE("Hide yo kids, hide yo wife!"),
+    CURRENTLYALLSERVERSAREOPERATING("Currently all servers are operating normally"),
+    INVESTIGATINGCAR1OFFLINE("Investigating: Car 1 is currently offline"),
+    WEATHERREPORTINCOMING("Updated Weather Report incoming. Please stand by"),
+    MAGICWORD("Ah, ah, ah. You didn't say the magic word"),
+    MALCOLM("Life finds a way"),
+    DEATHSTAR("The Death Star is fully operational"),
+    BOATAPPROACH("Boat 3 on approach to East Dock"),
+    BOATDEPART("Boat 5 departing on schedule"),
+
+
+
+}
